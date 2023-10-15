7c9d5b5c2404d735b6d666b65da042d3c4b403ee
Surafel Araya <surafelay@gmail.com>
2023-10-15 09:06:49 +0300

    Nested Routing

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import SideBar from "./sidebar";
+import Posts from "./posts";
+import Users from "./users";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <SideBar />
+      <Route path="/admin/posts" component={Posts} />
+      <Route path="/admin/users" component={Users} />
     </div>
   );
 };
